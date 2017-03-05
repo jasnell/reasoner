972f95aba3e6c9df8be4a5e55327e6c996e4a78e
James M Snell <jasnell@gmail.com>
2017-03-05 14:50:44 -0800

    Refactor API, updated dependencies

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ## Reasoner
-A simple linked data reasoner that uses N3 under the covers...
+A simple linked data reasoner...
 
 This module began life as part of the ActivityStrea.ms implementation
-(http://github.com/jasnell/activitystrea.ms). I am extracting it out as part of an effort to better modularize the ActivityStrea.ms project.
+(http://github.com/jasnell/activitystrea.ms).
 
-From v0.1.0 on, this module requires Node v4.0.0 / ES6. Also, the async
-callback versions of the methods have been removed in order to further
-simplify the API and implementation.
+### Requirements:
+
+This module requires Node.js 4.0.0 and uses ES6 language features.
 
 ### Install:
 ```
@@ -15,11 +15,11 @@ npm install reasoner
 
 ### Example:
 ```
-var rdf = require('linkeddata-vocabs').rdf;
-var Reasoner = require('reasoner');
-var Graph = Reasoner.Graph;
+const rdf = require('linkeddata-vocabs').rdf;
+const Reasoner = require('reasoner');
+const Graph = Reasoner.Graph;
 
-var graph = new Graph();
+const graph = new Graph();
 graph.add({
   subject: 'http://example.org/foo',
   predicate: rdf.type,
@@ -55,9 +55,10 @@ graph.add({
   predicate: rdfs.subClassOf,
   object: 'http://example.org/aaa'
 });
-var reasoner = new Reasoner(graph);
 
-var node = reasoner.node('http://example.org/foo');
+const reasoner = new Reasoner(graph);
+const node = reasoner.node('http://example.org/foo');
+
 node.is('http://example.org/bar'); // true
 node.is('http://example.org/boo'); // true
 node.is('http://example.org/aaa'); // true
@@ -69,7 +70,9 @@ node.is('http://example.org/bbb'); // false
 
 ### Constructor: `Reasoner([graph])`
 
-Creates a new Reasoner. Either a single `Reasoner.Graph` instance, or an array of `Reasoner.Graph` instances can be provided to the constructor to be bound automatically when the Reasoner is created.
+Creates a new Reasoner. Either a single `Reasoner.Graph` instance, or an array
+of `Reasoner.Graph` instances can be provided to the constructor to be bound
+automatically when the Reasoner is created.
 
 #### Property: `<number> Reasoner.prototype.size`
 
@@ -81,7 +84,8 @@ Binds a single `Reasoner.Graph` instance to the Reasoner.
 
 #### Method: `<Reasoner.Node> Reasoner.prototype.node(uri)`
 
-Returns a `Reasoner.Node` instance. A Node represents a single subject known to the Reasoner.
+Returns a `Reasoner.Node` instance. A Node represents a single subject known to
+the Reasoner.
 
 #### Method: `<Reasoner.Literal> Reasoner.prototype.literal(value[, options])`
 
@@ -89,7 +93,8 @@ Utility method that creates new `Reasoner.Literal`
 
 ### Constructor: `Reasoner.Graph()`
 
-Creates a new `Reasoner.Graph` instance. A Graph is essentially a thin wrapper around an `N3.Store` object.
+Creates a new `Reasoner.Graph` instance. A Graph is essentially a thin wrapper
+around an `N3.Store` object.
 
 #### Property : `<number> Reasoner.Graph.prototype.size`
 
@@ -97,11 +102,13 @@ Returns the total number of triples known to the Graph.
 
 #### Method: `Reasoner.Graph.prototype.add(triple)`
 
-Adds a single triple to the Graph. The argument is an object with three properties: `subject`, `predicate` and `object`.
+Adds a single triple to the Graph. The argument is an object with three
+properties: `subject`, `predicate` and `object`.
 
 ```javascript
-var Graph = require('reasoner').Graph;
-var graph = new Graph();
+const Graph = require('reasoner').Graph;
+const graph = new Graph();
+
 graph.add({
   subject: 'http://example.org/foo',
   predicate: 'http://example.org/bar',
@@ -118,34 +125,39 @@ Merge the specified Graph into this Graph.
 Search the Graph for triples matching the specified pattern.
 
 ```javascript
-var Graph = require('reasoner').Graph;
-var graph = new Graph();
+const Graph = require('reasoner').Graph;
+const graph = new Graph();
+
 graph.add({
   subject: 'http://example.org/foo',
   predicate: 'http://example.org/bar',
   object: 'http://example.org/baz'
 });
-var triples = graph.find({subject:'http://example.org/foo'});
-for (let triple of triples) {
+
+const triples = graph.find({subject:'http://example.org/foo'});
+for (const triple of triples) {
   console.log(triple);
 }
 ```
 
 ### Object: `Reasoner.Node`
 
-The `Reasoner.Node` object provides access to explicit and inferred information about a single subject known to the Reasoner.
+The `Reasoner.Node` object provides access to explicit and inferred information
+about a single subject known to the Reasoner.
 
 ```javascript
-var Reasoner = require('reasoner');
-var Graph = Reasoner.Graph;
-var graph = new Graph();
+const Reasoner = require('reasoner');
+const Graph = Reasoner.Graph;
+const graph = new Graph();
+
 graph.add({
   subject: 'http://example.org/foo',
   predicate: 'http://example.org/bar',
   object: 'http://example.org/baz'
 });
-var reasoner = new Reasoner(graph);
-var node = reasoner.node('http://example.org/foo');
+
+const reasoner = new Reasoner(graph);
+const node = reasoner.node('http://example.org/foo');
 // ...
 ```
 
@@ -155,11 +167,13 @@ The Subject URI
 
 #### Method: `Reasoner.Node.prototype.findInbound(predicate)`
 
-Searches for all triples known to the Reasoner for which this node is the *object*.
+Searches for all triples known to the Reasoner for which this node is the
+*object*.
 
 #### Method: `Reasoner.Node.prototype.find(predicate)`
 
-Searches for all triples known to the Reasoner for which this node is the *subject*.
+Searches for all triples known to the Reasoner for which this node is the
+*subject*.
 
 #### Method: `Reasoner.Node.prototype.literal(predicate)`
 
@@ -174,43 +188,45 @@ for (let literal of literals) {
 };
 ```
 
-#### Method: `Reasoner.Node.prototype.types()`
+#### Property: `Reasoner.Node.prototype.types`
 
-Returns the collection of rdf:type explicitly defined for this subject.
+The collection of `rdf:type` explicitly defined for this subject.
 
-#### Method: `Reasoner.Node.prototype.subProperties()`
+#### Property: `Reasoner.Node.prototype.subProperties`
 
-Returns a listing of `Reasoner.Node` objects representing other subjects defined as rdfs:subPropertyOf this subject.
+A listing of `Reasoner.Node` objects representing other subjects defined as
+`rdfs:subPropertyOf` this subject.
 
-#### Method: `Reasoner.Node.prototype.subClasses()`
+#### Property: `Reasoner.Node.prototype.subClasses`
 
-Returns a listing of `Reasoner.Node` objects representing other
-subjects defined as rdfs:subClassOf this subject.
+A listing of `Reasoner.Node` objects representing other
+subjects defined as `rdfs:subClassOf` this subject.
 
-#### Method: `Reasoner.Node.prototype.superProperties()`
+#### Property: `Reasoner.Node.prototype.superProperties`
 
-Returns a listing of `Reasoner.Node` objects representing other
+A listing of `Reasoner.Node` objects representing other
 subjects for which this node can be considered a sub-property.
 
-#### Method: `Reasoner.Node.prototype.superClasses()`
+#### Property: `Reasoner.Node.prototype.superClasses`
 
-Returns a listing of `Reasoner.Node` objects representing other
+A listing of `Reasoner.Node` objects representing other
 subjects for which this node can be considered a sub-class.
 
 #### Method: `Reasoner.Node.prototype.is(type)`
 
-Returns true if this subject can be considered to be an instance of the specified type. Specifically, this will return true if:
+Returns `true` if this subject can be considered to be an instance of the
+specified type. Specifically, this will return `true` if:
 
-1. It has rdf:type type
-2. One of it's rdf:type's is type
-3. It has rdfs:subClassOf type
-4. One of it's superClasses is type
-5. It has rdfs:subPropertyOf type
-6. One of it's superProperties is type
+1. It has `rdf:type` `type`
+2. One of it's `rdf:type`'s is `type`
+3. It has `rdfs:subClassOf` `type`
+4. One of it's superClasses is `type`
+5. It has `rdfs:subPropertyOf` `type`
+6. One of it's superProperties is `type`
 
 ### Object: `Reasoner.Literal`
 
-Represents a Literal value.
+Represents a literal value.
 
 #### Method: `Reasoner.Literal.prototype.valueOf()`
 
@@ -222,4 +238,5 @@ A `Reasoner.Node` representing the Literal type.
 
 #### Property: `<LanguageTag> Reasoner.Literal.prototype.language`
 
-A LanguageTag representing the Literal language. (The LanguageTag object is provided by the `rfc5646` module in npm)
+A `LanguageTag` representing the Literal language. (The `LanguageTag` object is
+provided by the `rfc5646` module in npm)
--- test/test.js
+++ test/test.js
@@ -1,258 +1,200 @@
-'use strict';
+/* global it describe */
+'use strict'
 
-const assert = require('assert');
-const async = require('async');
-const Graph = require('../lib/graph');
-const Reasoner = require('../lib/reasoner');
-const rdf = require('vocabs-rdf');
-const rdfs = require('vocabs-rdfs');
+const assert = require('assert')
+const Graph = require('../lib/graph')
+const Reasoner = require('../lib/reasoner')
+const rdf = require('vocabs-rdf')
+const rdfs = require('vocabs-rdfs')
 
-describe('It Works', function() {
-
-  it('Should work', function(done) {
-
-    var graph = new Graph();
+describe('It Works', () => {
+  it('Should work', (done) => {
+    const graph = new Graph()
     graph.add({
       subject: 'http://example.org/foo',
       predicate: 'http://example.org/bar',
       object: 'http://example.org/baz'
-    });
+    })
+    const reasoner = new Reasoner()
+    reasoner.bind(graph)
+    reasoner.bind(graph) // it's already been added
+    assert.strictEqual(reasoner.size, 57)
+    done()
+  })
 
-    var reasoner = new Reasoner();
-    reasoner.bind(graph);
-    reasoner.bind(graph); // it's already been added
-
-    assert.equal(reasoner.size, 57);
-    done();
-
-  });
-
-  it('Should merge', function(done) {
-
-    var graph1 = new Graph();
+  it('Should merge', (done) => {
+    const graph1 = new Graph()
     graph1.add({
       subject: 'http://example.org/foo',
       predicate: 'http://example.org/bar',
       object: 'http://example.org/baz'
-    });
-    var graph2 = new Graph();
+    })
+    const graph2 = new Graph()
     graph2.add({
       subject: 'http://example.org/foo1',
       predicate: 'http://example.org/bar1',
       object: 'http://example.org/baz1'
-    });
+    })
+    const graph3 = graph1.merge(graph2)
+    assert.strictEqual(graph3.size, 2)
+    done()
+  })
 
-    var graph3 = graph1.merge(graph2);
-    assert.equal(graph3.size,2);
-
-    done();
-  });
-
-  it('Should be reasonable...', function(done) {
-
-    var graph = new Graph();
+  it('Should be reasonable...', (done) => {
+    const graph = new Graph()
     graph.add({
       subject: 'http://example.org/words',
       predicate: rdfs.subPropertyOf,
       object: rdfs.ns + 'label'
-    });
+    })
     graph.add({
       subject: 'http://example.org/foo',
       predicate: rdfs.ns + 'label',
       object: '"testing"'
-    });
+    })
     graph.add({
       subject: 'http://example.org/foo',
       predicate: 'http://example.org/words',
       object: '"testing2"'
-    });
+    })
     graph.add({
       subject: 'http://example.org/foo',
       predicate: rdf.type,
       object: 'http://example.org/bar'
-    });
+    })
     graph.add({
       subject: 'http://example.org/foo',
       predicate: rdf.type,
       object: 'http://example.org/baz'
-    });
+    })
     graph.add({
       subject: 'http://example.org/foo',
       predicate: rdfs.subClassOf,
       object: 'http://example.org/boo'
-    });
+    })
     graph.add({
       subject: 'http://example.org/boo',
       predicate: rdfs.subClassOf,
       object: 'http://example.org/aaa'
-    });
+    })
     graph.add({
       subject: 'http://example.org/aaa',
       predicate: rdfs.subClassOf,
       object: 'http://example.org/ccc'
-    });
+    })
     graph.add({
       subject: 'http://example.org/bar',
       predicate: rdfs.subClassOf,
       object: 'http://example.org/aaa'
-    });
+    })
     graph.add({
       subject: 'http://example.org/bbb',
       predicate: rdfs.subClassOf,
       object: 'http://example.org/aaa'
-    });
-    var reasoner = new Reasoner(graph);
+    })
+    var reasoner = new Reasoner(graph)
+    const node = reasoner.node('http://example.org/foo')
 
-    var node = reasoner.node('http://example.org/foo');
-    async.parallel([
-      function(done) {
-        var types = node.types();
-        assert.equal(types.length,2);
-        types.forEach(function(item) {
-          assert(
-            item.id === 'http://example.org/bar' ||
-            item.id === 'http://example.org/baz'
-          );
-        });
-        done();
-      },
-      function(done) {
-        node.superClasses();
-        done();
-      },
-      function(done) {
-        var types = node.types();
-        assert.equal(types.length,2);
-        types.forEach(function(item) {
-          assert(
-            item.id === 'http://example.org/bar' ||
-            item.id === 'http://example.org/baz'
-          );
-        });
-        done();
-      },
-      function(done) {
-        var types = node.superClasses();
-        assert.equal(types.size,3);
-        types.forEach(function(item) {
-          assert(
-            item.id === 'http://example.org/boo' ||
-            item.id === 'http://example.org/aaa' ||
-            item.id === 'http://example.org/ccc'
-          );
-        });
-        done();
-      },
-      function(done) {
-        assert(node.is('http://example.org/bar'));
-        done();
-      },
-      function(done) {
-        assert(node.is('http://example.org/aaa'));
-        done();
-      },
-      function(done) {
-        assert(node.is('http://example.org/ccc'));
-        done();
-      },
-      function(done) {
-        assert(!node.is('http://example.org/bbb'));
-        done();
-      },
-      function(done) {
-        assert(node.is('http://example.org/bar'));
-        done();
-      },
-      function(done) {
-        assert(node.is('http://example.org/bar'));
-        done();
-      },
-      function(done) {
-        assert(node.is('http://example.org/aaa'));
-        done();
-      },
-      function(done) {
-        assert(node.is('http://example.org/ccc'));
-        done();
-      },
-      function(done) {
-        assert(!node.is('http://example.org/bbb'));
-        done();
-      },
-      function(done) {
-        var aaa = reasoner.node('http://example.org/aaa');
-        var types = aaa.subClasses();
-        assert.equal(types.size, 4);
-        types.forEach(function(m) {
-           assert(
-            m.id === 'http://example.org/bbb' ||
-            m.id === 'http://example.org/boo' ||
-            m.id === 'http://example.org/bar' ||
-            m.id === 'http://example.org/foo'
-           );
-        });
-        done();
-      },
-      function(done) {
-        var labels = node.literal(rdfs.ns + 'label');
-        assert.equal(labels.length, 2);
-        labels.forEach(function(label) {
-          assert(label == 'testing' ||
-                 label == 'testing2');
-        });
-        done();
+    assert(node.is('http://example.org/bar'))
+    assert(node.is('http://example.org/aaa'))
+    assert(node.is('http://example.org/ccc'))
+    assert(!node.is('http://example.org/bbb'))
+    assert(node.is('http://example.org/bar'))
+
+    {
+      const set = new Set([
+        'http://example.org/bar',
+        'http://example.org/baz'
+      ])
+      const types = node.types
+      assert.strictEqual(types.length, 2)
+      for (var n = 0; n < types.length; n++) {
+        assert(set.has(types[n].id))
       }
-    ], function() {
-      done();
-    });
+    }
 
-  });
+    {
+      const set = new Set([
+        'http://example.org/boo',
+        'http://example.org/aaa',
+        'http://example.org/ccc'
+      ])
+      const types = node.superClasses
+      assert.strictEqual(types.length, 3)
+      for (let n = 0; n < types.length; n++) {
+        assert(set.has(types[n].id))
+      }
+    }
 
-  it('should load the graph from the stream', function(done) {
-    var path = require('path');
-    var file = path.resolve(__dirname, 'test.ttl');
-    var fs = require('fs').createReadStream(file);
-    Graph.StreamLoader(fs, function(err, graph) {
-      assert.equal(err,undefined);
-      assert.equal(graph.size, 3);
-      done();
-    });
-  });
+    {
+      const set = new Set([
+        'http://example.org/bbb',
+        'http://example.org/boo',
+        'http://example.org/bar',
+        'http://example.org/foo'
+      ])
+      const aaa = reasoner.node('http://example.org/aaa')
+      const types = aaa.subClasses
+      assert.strictEqual(types.length, 4)
+      for (let n = 0; n < types.length; n++) {
+        assert(set.has(types[n].id))
+      }
+    }
 
-  it('should reduce properly', function(done) {
+    {
+      const set = new Set(['testing', 'testing2'])
+      const labels = node.literal(`${rdfs.ns}label`)
+      assert.strictEqual(labels.length, 3)
+      for (let n = 0; n < labels.length; n++) {
+        assert(set.has(`${labels[n]}`))
+      }
+    }
 
-    var graph = new Graph();
+    done()
+  })
+
+  it('should load the graph from the stream', (done) => {
+    const path = require('path')
+    const file = path.resolve(__dirname, 'test.ttl')
+    const fs = require('fs').createReadStream(file)
+    Graph.StreamLoader(fs, (err, graph) => {
+      assert(!err)
+      assert.strictEqual(graph.size, 3)
+      done()
+    })
+  })
+
+  it('should reduce properly', (done) => {
+    const graph = new Graph()
     graph.add({
       subject: 'http://example.org/foo',
       predicate: rdfs.subClassOf,
       object: 'http://example.org/aaa'
-    });
+    })
     graph.add({
       subject: 'http://example.org/bar',
       predicate: rdfs.subClassOf,
       object: 'http://example.org/foo'
-    });
+    })
     graph.add({
       subject: 'http://example.org/baz',
       predicate: rdfs.subClassOf,
       object: 'http://example.org/aaa'
-    });
-    var reasoner = new Reasoner(graph);
-    var types = [
+    })
+    const reasoner = new Reasoner(graph)
+    const types = [
       'http://example.org/foo',
       'http://example.org/bar',
       'http://example.org/baz',
       'http://example.org/baz',
       'http://example.org/aaa'
-    ];
-    var res = reasoner.reduce(types);
-    assert.equal(res.length,2);
-    res.forEach(function(type) {
-      assert(
-        type === 'http://example.org/bar' ||
-        type === 'http://example.org/baz'
-      );
-    });
-    done();
-  });
-});
+    ]
+    const res = reasoner.reduce(types)
+    assert.strictEqual(res.length, 2)
+    assert.deepStrictEqual(res, [
+      'http://example.org/bar',
+      'http://example.org/baz'
+    ])
+    done()
+  })
+})
